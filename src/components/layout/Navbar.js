bd6943875696318e214ab3b54ff4bb6e48eb8321
Ryan Conry <rconry@clearpath.ai>
2019-12-24 10:04:44 -0500

    create Navbar, User and UserItem components

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Navbar = ({ icon, title }) => {
+
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                <i className={icon} ></i> {title}
+            </h1>
+        </nav>
+    )
+}
+
+Navbar.defaultProps = {         //default props for func component
+    title: 'Github Finder',
+    icon: 'fas fa-github'
+};
+
+Navbar.propTypes = {            //propTypes for func component
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
+
+export default Navbar
