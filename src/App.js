26a887e73fabe4d19030b6a7d04f9e7419801498
Ryan Conry <r.conry@hotmail.com>
2019-12-24 11:46:37 -0500

    fixed navbar destructuring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css';
 import NavBar from './components/layout/Navbar'
 import Users from './components/users/Users'
 
 export default class App extends Component {
+  state = {
+    loading: false
+  };
+  componentDidMount() {
+    this.setState({ loading: true })
+    axios.get('https://api.github.com/users').then(res => this.setState({ users: res.data }))
+    this.setState({ loading: false })
+  }
+
   render() {
     return (
       <div className='App'>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,15 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
-const Navbar = ({ icon, title }) => {
 
+const Navbar = ({ icon, title }) => {
     return (
         <nav className='navbar bg-primary'>
             <h1>
-                <i className={icon} ></i> {title}
+                <i className={icon} /> {title}
             </h1>
         </nav>
-    )
+
+    );
 }
 
 Navbar.defaultProps = {         //default props for func component
