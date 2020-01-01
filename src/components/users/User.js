045c17ab8ba69ab487b0e16d6e04f4f6d2fbddce
Ryan Conry <r.conry@hotmail.com>
2020-01-01 17:08:45 -0500

    build out user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,75 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react';
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
 
 class User extends Component {
+    componentDidMount() {
+        this.props.getUser(this.props.match.params.login) // pulls login from /user/:login URL an passes to getUser
+    }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
+    }
+
     render() {
-        console.log('here')
+        const { name,
+            avatar_url,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            company,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable } = this.props.user;
+
+        const { loading } = this.props;
+
+        if (loading) return <Spinner />;
+
         return (
-            <div>
-                User
-            </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light'> Back To Search</Link>
+                Hireable: {' '} {hireable ? <i className='fas fa-check text-success' /> : <i className='fas fa-times-circle text-danger' />}
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} alt="" className="round-img" style={{ width: '150px' }} />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (<Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>)}
+                        <a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
+                        <ul>
+                            <li>
+                                {login && <Fragment>
+                                    <strong>Username: </strong> {login}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {company && <Fragment>
+                                    <strong>Company: </strong> {company}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {blog && <Fragment>
+                                    <strong>Blog: </strong> {blog}
+                                </Fragment>}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </Fragment>
         );
     }
 }
